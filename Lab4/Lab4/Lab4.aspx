59613226895438422dc27a1297dbd6b9de907a5e
Neil <frankenstein68k@gmail.com>
2014-07-28 20:30:17 -0400

    try 2

---
+++ Lab4/Lab4/Lab4.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Lab4.aspx.cs" Inherits="Lab4.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+
+    Page Load Time
+    <asp:Label ID="lbl1" runat="server" />
+
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            
+            <div>
+                <label for="ddlSupplier">Please Choose A Supplier</label>
+                <asp:DropDownList ID="ddlSupplier" runat="server" DataTextField="CompanyName" DataValueField="SupplierID" />
+            </div>
+            <asp:Button ID="Button1" runat="server" Text="Get Products" OnClick="Button1_Click" />
+            <div>
+                Gridview Load Time
+                <asp:Label ID="lbl2" runat="server" />
+                <asp:GridView ID="grdProducts" runat="server" AutoGenerateColumns="false">
+                    <Columns>
+                        <asp:BoundField HeaderText="Product Name" Datafield="ProductName" />
+                        <asp:BoundField HeaderText="Unit Price" DataField="UnitPrice" />
+                        <asp:BoundField HeaderText="Product ID" DataField="ProductID" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ Lab4/Lab4/Lab4.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                getSuppliers();
+            }
+            lbl1.Text = DateTime.Now.ToLongTimeString();
+        }
+
+
+        protected void getSuppliers()
+        {
+            using (NorthwindEntities objEnt = new NorthwindEntities())
+            {
+                var data = from Supplier in objEnt.Suppliers
+                           orderby Supplier.CompanyName
+                           select Supplier;
+
+                ddlSupplier.DataSource = data.ToList();
+                ddlSupplier.DataBind();
+            }
+        }
+
+        protected void getProducts()
+        {
+            int SupplierID = int.Parse(ddlSupplier.SelectedValue);
+
+            using (NorthwindEntities objEnt = new NorthwindEntities())
+            {
+                var data = from Product in objEnt.Products
+                           where Product.SupplierID == SupplierID
+                           orderby Product.ProductName
+                           select Product;
+
+                grdProducts.DataSource = data.ToList();
+                grdProducts.DataBind();
+            }
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            getProducts();
+            lbl2.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
